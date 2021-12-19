1480572510905087e7fd37078df9c44185fcd27e
iTBoyer <iTBoyer@qq.com>
2021-12-19 08:56:07 +0800

    update

---
+++ iHacker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iHacker
+//
+//  Created by boyer on 2021/12/17.
+//
+
+import SwiftUI
+import RemoteImageView
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iHacker/iHackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHackerApp.swift
+//  iHacker
+//
+//  Created by boyer on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct iHackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
