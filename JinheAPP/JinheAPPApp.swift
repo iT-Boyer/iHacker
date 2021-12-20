e649f10b5aff1bb0121a3354b1b36ce716abd73a
iTBoyer <iTBoyer@qq.com>
2021-12-20 10:34:07 +0800

    添加jinheappdemo

---
+++ JinheAPP/JinheAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  JinheAPPApp.swift
+//  JinheAPP
+//
+//  Created by boyer on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct JinheAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
