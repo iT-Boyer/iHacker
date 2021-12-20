d146db7a2034525d0b9cd4fd5b09d424fe3f8a9a
iTBoyer <iTBoyer@qq.com>
2021-12-20 11:28:39 +0800

    替换程序启动入口

--- JinheAPP/ViewController.swift
+++ JinheAPP/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        let tetlab = UILabel()
+        tetlab.text = "第一步"
+        tetlab.textAlignment = .center
+        tetlab.textColor = UIColor.red
+        self.view.addSubview(tetlab)
+        tetlab.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalTo(40)
+            make.center.equalTo(self.view)
+        }
     }
 }
 
