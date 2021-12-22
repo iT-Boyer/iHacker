44171823e09a87a9493a392edb904a3ba29d99f4
iTBoyer <iTBoyer@qq.com>
2021-12-22 14:25:13 +0800

    update

--- iHacker/Landmarks/Views/LandmarkRow.swift
+++ iHacker/Landmarks/Views/LandmarkRow.swift
@@ -24,7 +24,21 @@ struct LandmarkRow:View
 
 struct LandmarkRow_Previews:PreviewProvider
 {
+    // previews 可以返回多个View someview
+    // previewLayout 预览视图布局方法。辅助在开发中的多个视图的布局样式。
+    // Group容器，可以批量处理多个View的样式。
     static var previews:some View{
+        
         LandmarkRow(landmark: landmarks[0])
+            .previewLayout(.fixed(width: 390, height: 50))
+        LandmarkRow(landmark: landmarks[1])
+            .previewLayout(.fixed(width: 390, height: 50))
+        
+        //使用group分组，group的设置会作用到每一个View上
+        Group{
+            LandmarkRow(landmark: landmarks[2])
+            LandmarkRow(landmark: landmarks[3])
+        }.previewLayout(.fixed(width: 390, height: 50))
+        
     }
 }
