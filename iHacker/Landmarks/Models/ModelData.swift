9875cd5e88126f53a3ac5d59f71cd65af0432d9e
iTBoyer <iTBoyer@qq.com>
2021-12-22 13:28:07 +0800

    功能点：梳理swiftIU添加cell和模型的方式 问题：

---
+++ iHacker/Landmarks/Models/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  iHacker
+//
+//  Created by boyer on 2021/12/22.
+//
+
+import Foundation
+
+var landmarks:[Landmark] = load("landmarkData.json")
+
+func load<T:Decodable>(_ filename:String) -> T
+{
+    let data: Data
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else{
+        fatalError("加载 \(filename)失败")
+    }
+
+    do{
+        data = try Data(contentsOf: file)
+    }catch{
+        fatalError("加载\(filename)失败：\(error)")
+    }
+
+    do{
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }catch{
+        fatalError("\(filename)解析失败：\(error)")
+    }
+}
