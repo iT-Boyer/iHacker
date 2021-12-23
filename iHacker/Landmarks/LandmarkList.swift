83dd2398bf80f1f02bcaf5b804ef7f47921ad3a4
iTBoyer <iTBoyer@qq.com>
2021-12-23 16:02:50 +0800

    实现仅展示收藏的item，添加开关筛选条件和toggle视图绑定一个State属性，实现实时更新list列表

--- iHacker/Landmarks/LandmarkList.swift
+++ iHacker/Landmarks/LandmarkList.swift
@@ -9,14 +9,48 @@ import SwiftUI
 import MapKit
 
 struct LandmarkList: View {
+    // 仅展示收藏的row
+    @State var showFavoritesOnly = false
+    
+    func filterFavorites() -> [Landmark] {
+        // 筛选返回收藏的数据源
+        let filterArr = landmarks.compactMap { landmark -> Landmark? in
+            let isFavorite = landmark.isFavorite
+            //仅展示
+            if showFavoritesOnly{
+                if isFavorite {
+                    return landmark
+                }else{
+                    return nil
+                }
+            }else{
+                return landmark
+            }
+        }
+        return filterArr
+    }
+    
+    var filterLandmarks:[Landmark]{
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     
     var body: some View {
         NavigationView{
-            List(landmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark:landmark)
+            VStack{
+                // 开关
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("")
+                }.padding()
+                
+                // 列表
+                List(filterLandmarks){ landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark:landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -28,6 +62,8 @@ struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         
         return LandmarkList()
+        
+        
         //ForEach 以与 list 相同的方式对集合进行操作,这样我们就可以在任何可以使用子视图的地方使用它，比如 stacks ， lists ，groups 等。
         //当数据元素像这里使用的字符串一样是简单的值类型时，我们可以使用 \.self 作为标识符的 key path 。
         ForEach(["iPhone 13 Pro Max", "iPhone 13 Pro"], id: \.self) { deviceName in
