04568e26066c24880688bdeacf5d178fb2a7c539
iTBoyer <iTBoyer@qq.com>
2021-12-29 19:00:58 +0800

    混编模块调研

---
+++ iWorker/RuntimeCmd/Teacher.swift
@@ -0,0 +1,15 @@
+//
+//  Teacher.swift
+//  RuntimeCmd
+//
+//  Created by boyer on 2021/12/29.
+//
+
+import Foundation
+
+public class Teacher:NSObject {
+
+    @objc func teach() {
+        print(#function)
+    }
+}
---
+++ iWorker/RuntimeCmd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeCmd
+//
+//  Created by boyer on 2021/12/29.
+//
+
+#import <Foundation/Foundation.h>
+#import "RuntimeCmd-Swift.h"
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        Teacher *teacher = [Teacher new];
+        [teacher teach];
+    }
+    return 0;
+}
