8d95277e6e9f4e5d09f5c8baeea1e05317e68fbb
iTBoyer <iTBoyer@qq.com>
2022-01-25 18:08:32 +0800

    update

--- iWorker/iWorker/连锁集团/JHUnitOrgAlertController.swift
+++ iWorker/iWorker/连锁集团/JHUnitOrgAlertController.swift
@@ -7,6 +7,37 @@
 
 import UIKit
 
-class JHUnitOrgAlertController: UIViewController {
+enum JHAlertControllerStyle {
+    case JHAlertControllerStyleAlert, JHAlertControllerStyleSheet, JHAlertControllerStyleToast
+}
 
+class JHUnitOrgAlertController: UIViewController {
+    
+    private var _alertView: UIView!
+    private var _alertTitle: UILabel!
+    private var _alertMessage: UILabel!
+    private var _alertActionStackView: UIStackView!
+    var style: JHAlertControllerStyle
+    
+    init(title: String!, message: String!, image: UIImage!, style: JHAlertControllerStyle) {
+        self.style = style
+        super.init(nibName: nil, bundle: nil)
+        self.modalPresentationStyle = .overCurrentContext
+        self.modalTransitionStyle = .coverVertical
+        createView()
+        _alertTitle.text = title;
+        _alertMessage.text = message;
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    func createView() {
+        
+    }
 }
